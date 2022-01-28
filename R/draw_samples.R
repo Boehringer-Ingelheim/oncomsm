0c62b8c3572810ce788318ec0761bf9fbadc25f2
Kevin Kunzmann <kevin.kunzmann@boehringer-ingelheim.com>
2022-01-28 14:56:39 +0100

    renaming, S3 stuff

---
+++ R/draw_samples.R
@@ -0,0 +1,16 @@
+#' Draw samples from a model
+#'
+#' simulate exact response times from the specified model,
+#' optionally conditioning on additional data.
+#'
+#' @param model the model to simulate from
+#' @param n_to_be_recruited int, number of individuals to sample for; these are
+#' future individuals and additional the (optional) individuals provided via the "data" argument.
+#' @param nsim int >= 10L, number of samples to draw
+#' @param data **optional** data to condition on
+#' @param seed **optional** random seed for simulation
+#'
+#' @export
+draw_samples <- function(model, n_to_be_recruited, nsim, data = NULL, seed = NULL, ...) {
+  UseMethod("draw_samples")
+}
