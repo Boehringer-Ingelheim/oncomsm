286b1d89454f262af6196be1f40b99ab5608f32f
Ananthakrishnan <karthik.ananthakrishnan@boehringer-ingelheim.com>
2022-10-21 13:01:50 +0200

    test on posterior sampling 1

--- tests/testthat/test-srp_model.R
+++ tests/testthat/test-srp_model.R
@@ -3,7 +3,7 @@ test_that("can create SRP model", {
   mdl <<- create_srp_model(
     group_id = 1:2,
     logodds_mean =  c(logodds(.5), logodds(.5)),
-    logodds_sd = c(.5, .5),
+    logodds_sd = c(.1, .1),
     visit_spacing = c(1.2, 1.2),
     median_time_to_next_event = matrix(c(
       3, 3, 6,
@@ -149,7 +149,7 @@ test_that("can generate data from SRP model", {
   tbl_prior_predictive1 <<- sample_predictive(
     mdl,
     n_per_group = c(20, 20),
-    nsim = 25,
+    nsim = 100,
     seed = 42L
   )
 
@@ -184,7 +184,7 @@ test_that("prior predictive seed works", {
   tbl_prior_predictive2 <- sample_predictive(
     mdl,
     n_per_group = c(20, 20),
-    nsim = 25,
+    nsim = 100,
     seed = 42L
   )
   expect_true(all(tbl_prior_predictive1 == tbl_prior_predictive2))
@@ -209,13 +209,13 @@ test_that("can sample from posterior", {
     filter(iter == 1) %>%
     select(-iter)
 
-  p_obs <- tbl_data %>%
+  p_obs <- tbl_prior_predictive1 %>%
     filter(from == "stable") %>%
     group_by(group_id) %>%
     summarize(p = mean(to == "response")) %>%
     pull(p)
 
-  smpl_posterior <- sample_posterior(mdl, tbl_data)
+  smpl_posterior <- sample_posterior(mdl, tbl_prior_predictive1)
 
   p <- parameter_sample_to_tibble(mdl, smpl_posterior) %>%
     filter(parameter == "p") %>%
