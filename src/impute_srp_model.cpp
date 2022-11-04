e8be13475d2f494dec0a171463073a32786d6208
AKktk <93273026+AKktk@users.noreply.github.com>
2022-11-04 13:03:33 +0100

    Enhance speed of r-cpp sampler (#25)
    * pre-allocating memory for c++ implementation
    * adding Karthik as contributor
    
    Co-authored-by: Kunzmann,Kevin (MED BDS) BIP-DE-I <kevin.kunzmann@boehringer-ingelheim.com>

--- src/impute_srp_model.cpp
+++ src/impute_srp_model.cpp
@@ -56,16 +56,16 @@ DataFrame impute_srp_model(
     shape[idx[0] - 1][idx[1] - 1][idx[2] - 1] = shape_vec[i];
     scale[idx[0] - 1][idx[1] - 1][idx[2] - 1] = scale_vec[i];
   }
-  // define output vectors of length 0
-  IntegerVector subject_id_out (0);
-  IntegerVector group_id_out (0);
-  CharacterVector from_out (0);
-  CharacterVector to_out (0);
-  NumericVector t_min_out (0);
-  NumericVector t_max_out (0);
-  NumericVector t_sot_out (0);
-  IntegerVector iter_out (0);
+  IntegerVector subject_id_out (2*df.nrows()*n_sim);
+  IntegerVector group_id_out (2*df.nrows()*n_sim);
+  CharacterVector from_out (2*df.nrows()*n_sim);
+  CharacterVector to_out (2*df.nrows()*n_sim);
+  NumericVector t_min_out (2*df.nrows()*n_sim);
+  NumericVector t_max_out (2*df.nrows()*n_sim);
+  NumericVector t_sot_out (2*df.nrows()*n_sim);
+  IntegerVector iter_out (2*df.nrows()*n_sim);
   // iterate over number of samples to draw
+  int row_idx = 0;
   for (int iter = 0; iter < n_sim; iter++) {
     // iterate over rows in data
     for (int i = 0; i < df.nrows(); i++) {
@@ -73,24 +73,25 @@ DataFrame impute_srp_model(
       bool target_state_known = !CharacterVector::is_na(to[i]);
       if (target_state_known) {
         // nothing to sample - target state is observed, copy for each iteration
-        subject_id_out.push_back(subject_id(i));
-        group_id_out.push_back(group_id(i));
-        from_out.push_back(from(i));
-        to_out.push_back(to(i));
-        t_min_out.push_back(t_min(i));
-        t_max_out.push_back(t_max(i));
-        t_sot_out.push_back(t_sot(i));
-        iter_out.push_back(iter + 1);
+        subject_id_out[row_idx] = subject_id(i);
+        group_id_out[row_idx] = group_id(i);
+        from_out[row_idx] = from(i);
+        to_out[row_idx] = to(i);
+        t_min_out[row_idx] = t_min(i);
+        t_max_out[row_idx] = t_max(i);
+        t_sot_out[row_idx] = t_sot(i);
+        iter_out[row_idx] = iter + 1;
+        row_idx++;
       } else {
         if (from(i) == "stable") {
           // first sample whether response or progression happens
           double pr_response = conditional_response_probability_srp(
-              t_min(i) - t_sot(i), // (min) time since start of treatment
-              p(iter, g), // unconditional response probability
-              // transition index -1 due to 0 based indexing in C
-              shape[iter][g][0], shape[iter][g][1],
-              scale[iter][g][0], scale[iter][g][1]
-            );
+            t_min(i) - t_sot(i), // (min) time since start of treatment
+            p(iter, g), // unconditional response probability
+            // transition index -1 due to 0 based indexing in C
+            shape[iter][g][0], shape[iter][g][1],
+                                             scale[iter][g][0], scale[iter][g][1]
+          );
           bool response = R::rbinom(1, pr_response);
           if (response) {
             double dt = rtruncweibull(
@@ -109,53 +110,57 @@ DataFrame impute_srp_model(
             // check corner case where transitions are in same visit interval
             if (tmin_prog == tmin) {
               // append to results vectors stable -> progression
-              subject_id_out.push_back(subject_id(i));
-              group_id_out.push_back(group_id(i));
-              from_out.push_back("stable");
-              to_out.push_back("progression");
-              t_min_out.push_back(tmin);
-              t_max_out.push_back(tmax);
-              t_sot_out.push_back(t_sot(i));
-              iter_out.push_back(iter + 1);
+              subject_id_out[row_idx] = subject_id(i);
+              group_id_out[row_idx] = group_id(i);
+              from_out[row_idx] = "stable";
+              to_out[row_idx] = "progression";
+              t_min_out[row_idx] = tmin;
+              t_max_out[row_idx] = tmax;
+              t_sot_out[row_idx] = t_sot(i);
+              iter_out[row_idx] = iter + 1;
+              row_idx++;
             } else {
               // append to results vectors stable -> response
-              subject_id_out.push_back(subject_id(i));
-              group_id_out.push_back(group_id(i));
-              from_out.push_back("stable");
-              to_out.push_back("response");
-              t_min_out.push_back(tmin);
-              t_max_out.push_back(tmax);
-              t_sot_out.push_back(t_sot(i));
-              iter_out.push_back(iter + 1);
+              subject_id_out[row_idx] = subject_id(i);
+              group_id_out[row_idx] = group_id(i);
+              from_out[row_idx] = "stable";
+              to_out[row_idx] = "response";
+              t_min_out[row_idx] = tmin;
+              t_max_out[row_idx] = tmax;
+              t_sot_out[row_idx] = t_sot(i);
+              iter_out[row_idx] = iter + 1;
+              row_idx++;
               //append to results vectors response -> progression
-              subject_id_out.push_back(subject_id(i));
-              group_id_out.push_back(group_id(i));
-              from_out.push_back("response");
-              to_out.push_back("progression");
-              t_min_out.push_back(tmin_prog);
-              t_max_out.push_back(tmax_prog);
-              t_sot_out.push_back(t_sot(i));
-              iter_out.push_back(iter + 1);
+              subject_id_out[row_idx] = subject_id(i);
+              group_id_out[row_idx] = group_id(i);
+              from_out[row_idx] = "response";
+              to_out[row_idx] = "progression";
+              t_min_out[row_idx] = tmin_prog;
+              t_max_out[row_idx] = tmax_prog;
+              t_sot_out[row_idx] = t_sot(i);
+              iter_out[row_idx] = iter + 1;
+              row_idx++;
             }
           } else {
             // non-responder (directly to progression)
             // sample exact time from stable to progression
             double dt = rtruncweibull(
-                shape[iter][g][1], scale[iter][g][1], t_min(i), R_PosInf
-              );
+              shape[iter][g][1], scale[iter][g][1], t_min(i), R_PosInf
+            );
             // apply visit scheme
             double n_visits = floor(dt / visit_spacing(g));
             double tmin = t_min(i) + visit_spacing(g) * n_visits;
             double tmax = t_min(i) + visit_spacing(g) * (n_visits + 1);
             // append to results vectors
-            subject_id_out.push_back(subject_id(i));
-            group_id_out.push_back(group_id(i));
-            from_out.push_back("stable");
-            to_out.push_back("progression");
-            t_min_out.push_back(tmin);
-            t_max_out.push_back(tmax);
-            t_sot_out.push_back(t_sot(i));
-            iter_out.push_back(iter + 1);
+            subject_id_out[row_idx] = subject_id(i);
+            group_id_out[row_idx] = group_id(i);
+            from_out[row_idx] = "stable";
+            to_out[row_idx] = "progression";
+            t_min_out[row_idx] = tmin;
+            t_max_out[row_idx] = tmax;
+            t_sot_out[row_idx] = t_sot(i);
+            iter_out[row_idx] = iter + 1;
+            row_idx++;
           }
         }
         if (from(i) == "response") {
@@ -167,25 +172,36 @@ DataFrame impute_srp_model(
             shape[iter][g][0], scale[iter][g][0], t_min(i-1), t_max(i-1)
           );
           double dt_progression = rtruncweibull(
-              shape[iter][g][2], scale[iter][g][2],
-                                               t_min(i) - t_response, R_PosInf
+            shape[iter][g][2], scale[iter][g][2],
+                                             t_min(i) - t_response, R_PosInf
           );
           double n_visits_prog = floor(dt_progression / visit_spacing(g));
           double tmin_prog = t_min(i) + visit_spacing(g) * n_visits_prog;
           double tmax_prog = t_min(i) + visit_spacing(g) * (n_visits_prog + 1);
           // append results
-          subject_id_out.push_back(subject_id(i));
-          group_id_out.push_back(group_id(i));
-          from_out.push_back("response");
-          to_out.push_back("progression");
-          t_min_out.push_back(tmin_prog);
-          t_max_out.push_back(tmax_prog);
-          t_sot_out.push_back(t_sot(i));
-          iter_out.push_back(iter + 1);
+          subject_id_out[row_idx] = subject_id(i);
+          group_id_out[row_idx] = group_id(i);
+          from_out[row_idx] = "response";
+          to_out[row_idx] = "progression";
+          t_min_out[row_idx] = tmin_prog;
+          t_max_out[row_idx] = tmax_prog;
+          t_sot_out[row_idx] = t_sot(i);
+          iter_out[row_idx] = iter + 1;
+          row_idx++;
         }
       }
     }
   }
+
+  subject_id_out = subject_id_out[Rcpp::Range(0,(row_idx-1))];
+  group_id_out = group_id_out[Rcpp::Range(0,(row_idx-1))];
+  t_min_out = t_min_out[Rcpp::Range(0,(row_idx-1))];
+  t_max_out = t_max_out[Rcpp::Range(0,(row_idx-1))];
+  t_sot_out = t_sot_out[Rcpp::Range(0,(row_idx-1))];
+  iter_out = iter_out[Rcpp::Range(0,(row_idx-1))];
+
+  to_out = to_out[Rcpp::Range(0,(row_idx-1))];
+  from_out = from_out[Rcpp::Range(0,(row_idx-1))];
   // combine to return DataFrame
   DataFrame res = DataFrame::create(
     Named("subject_id") = subject_id_out,
